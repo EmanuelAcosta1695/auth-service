ff5a0b32dcbe3ece64cde5d1a9a58c7689ec7055
Emanuel Acosta <e.acosta1695@gmail.com>
2024-10-14 12:51:26 -0300

    feat: add email verification functionality

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -4,7 +4,7 @@ import { Loader, Lock, Mail, User } from "lucide-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
-// import { useAuthStore } from "../store/authStore";
+import { useAuthStore } from "../store/authStore";
 
 const SignUpPage = () => {
 	const [name, setName] = useState("");
@@ -12,17 +12,17 @@ const SignUpPage = () => {
 	const [password, setPassword] = useState("");
 	const navigate = useNavigate();
 
-	// const { signup, error, isLoading } = useAuthStore();
+	const { signup, error, isLoading } = useAuthStore();
 
 	const handleSignUp = async (e) => {
 		e.preventDefault();
 
-		// try {
-		// 	await signup(email, password, name);
-		// 	navigate("/verify-email");
-		// } catch (error) {
-		// 	console.log(error);
-		// }
+		try {
+			await signup(email, password, name);
+			navigate("/verify-email");
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	return (
@@ -60,7 +60,7 @@ const SignUpPage = () => {
 						value={password}
 						onChange={(e) => setPassword(e.target.value)}
 					/>
-					{/* {error && <p className='text-red-500 font-semibold mt-2'>{error}</p>} */}
+					{error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
 					<PasswordStrengthMeter password={password} />
 
 					<motion.button
@@ -71,10 +71,9 @@ const SignUpPage = () => {
 						whileHover={{ scale: 1.02 }}
 						whileTap={{ scale: 0.98 }}
 						type='submit'
-						disabled={false}
+						disabled={isLoading}
 					>
-            Sign Up
-						{/* {isLoading ? <Loader className=' animate-spin mx-auto' size={24} /> : "Sign Up"} */}
+						{isLoading ? <Loader className=' animate-spin mx-auto' size={24} /> : "Sign Up"}
 					</motion.button>
 				</form>
 			</div>
